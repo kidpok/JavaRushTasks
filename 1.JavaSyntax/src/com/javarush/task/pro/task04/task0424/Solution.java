2fbf5b70f5abadecf8d6256a4b20f025839ae4c7
macbook <babayev.nicat@yandex.ru>
2023-01-18 17:56:33 +0400

    add solution JavaSyntax task 0418

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0424/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0424/Solution.java
@@ -10,11 +10,12 @@ public class Solution {
     public static void main(String[] args) {
         Scanner bombInterface = new Scanner(System.in);
 
-
         int i;
+        i = bombInterface.nextInt();
         do {
-            i = bombInterface.nextInt();
+
             System.out.println(i);
+
         } while ((i % 10) != 0);
 
         //напишите тут ваш код
