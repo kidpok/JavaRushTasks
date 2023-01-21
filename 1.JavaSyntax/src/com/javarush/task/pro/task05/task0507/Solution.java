dc5878cc550aab982c6aef52aea821a20fa95c57
macbook <babayev.nicat@yandex.ru>
2023-01-21 16:38:04 +0400

    add solution JavaSyntax task 0507

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0507/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0507/Solution.java
@@ -10,6 +10,20 @@ public class Solution {
     public static int[] array;
 
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+
+        Scanner console = new Scanner(System.in);
+
+        int number = console.nextInt();
+        array = new int[number];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = console.nextInt();
+        }
+        int max = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if ( array[i] > max){
+                max = array[i];
+            }
+        }
+        System.out.println(max);
     }
 }
