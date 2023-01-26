f3f6b5358359370ae87c726722a855ff75facaaa
macbook <babayev.nicat@yandex.ru>
2023-01-26 17:48:16 +0400

    add solution JavaSyntax task 0701

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0701/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.pro.task07.task0701;
+
+/* 
+Все возможные типы
+*/
+
+public class Solution {
+    int a = 1;
+    byte i = 10;
+    long j = 125;
+    short f = 1234;
+    boolean b = true;
+    char chr = 10;
+    double db = 1.2;
+    float fl = 1.3f;
+
+    //напишите тут ваш код
+}
