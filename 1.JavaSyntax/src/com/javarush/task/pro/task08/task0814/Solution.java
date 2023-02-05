7bc86376a437acda650c0f942db9691617059a5d
macbook <babayev.nicat@yandex.ru>
2023-02-05 15:22:35 +0400

    add solution JavaSyntax task 0814

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task08/task0814/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.pro.task08.task0814;
+
+/* 
+Флаги
+*/
+
+public class Solution {
+
+    public static int setFlag(int number, int flagPos) {
+        number = number | (1 << flagPos);
+        return number;
+    }
+
+    public static int resetFlag(int number, int flagPos) {
+        number = number & ~(1 << flagPos);
+        return number;
+    }
+
+    public static boolean checkFlag(int number, int flagPos) {
+        if ((number & (1 << flagPos)) == (1 << flagPos)){
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+}
+
