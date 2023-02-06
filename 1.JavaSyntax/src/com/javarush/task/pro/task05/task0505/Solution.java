94df20af4a64530baae05131b10dc33be75a44dc
macbook <babayev.nicat@yandex.ru>
2023-02-06 14:52:24 +0400

    add solution JavaSyntax task 0505

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0505/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0505/Solution.java
@@ -1,5 +1,6 @@
 package com.javarush.task.pro.task05.task0505;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 /* 
@@ -9,5 +10,26 @@ Reverse
 public class Solution {
     public static void main(String[] args) {
         //напишите тут ваш код
+        Scanner console = new Scanner(System.in);
+        int number = console.nextInt();
+        int[] a = new int[number];
+        for (int i = 0; i < a.length; i++) {
+            if (number > 0) {
+                a[i] = console.nextInt();
+            } else break;
+        }
+        if (number % 2 == 0) {
+            for (int i = a.length - 1; i >= 0; i--) {
+                System.out.println(a[i]);
+            }
+        } else {
+            for (int i = 0; i < a.length; i++) {
+                System.out.println(a[i]);
+            }
+        }
     }
 }
+
+
+
+
