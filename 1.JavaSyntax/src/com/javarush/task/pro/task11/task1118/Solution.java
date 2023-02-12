837d49a3ba3f238aa80fe63b690e8538460af1e0
macbook <babayev.nicat@yandex.ru>
2023-02-12 20:39:57 +0400

    add solution JavaSyntax task 1118

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task11/task1118/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task11.task1118;
+
+import java.util.Scanner;
+
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        if (console.hasNext()) {
+            System.out.println(console.next());
+        }
+    }
+}
