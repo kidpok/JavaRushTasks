2f04a8b4f014bc21a2b75b55a910c1bec6f0e673
macbook <babayev.nicat@yandex.ru>
2023-02-15 00:06:46 +0400

    add solution JavaSyntax task 1007s

--- 1.JavaSyntax/src/com/javarush/task/pro/task11/task1124/Coffee.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task11/task1124/Coffee.java
@@ -1,8 +1,7 @@
 package com.javarush.task.pro.task11.task1124;
 
 /* 
-MacCoffee 3 в 1
-*/
+
 
 public class Coffee {
     public static class Milk {
@@ -10,5 +9,5 @@ public class Coffee {
     }
 }
 
- class Sugar {
-}
+ class œSugar {
+} */
