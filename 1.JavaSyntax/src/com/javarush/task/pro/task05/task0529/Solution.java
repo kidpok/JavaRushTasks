931f260bcf09bb63b5fc324b3647a929cb478ee3
macbook <babayev.nicat@yandex.ru>
2023-02-17 02:00:17 +0400

    add solution JavaSyntax task 0529

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0529/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0529/Solution.java
@@ -13,23 +13,36 @@ public class Solution {
     public static int width = 30;
     public static int height = 10;
     public static String[][] field = new String[height][width];
+    public static int[][] bombs = new int[height][width];
 
-      public static void main(String[] args) {
+    public static void main(String[] args) {
 
-        for (int i = 0; i < field.length ; i++) {
+        for (int i = 0; i < field.length; i++) {
             for (int j = 0; j < field[i].length; j++) {
                 field[i][j] = empty;
             }
         }
-          for (int i = 0; i < field.length; i++) {
-              int x = (int) (Math.random() * width);
-              field[i][x] = robotank;
-          }
-          for (int i = 0; i < field.length; i++) {
-              for (int j = 0; j < field[i].length ; j++) {
-                  System.out.print(field[i][j]);
-              }
-              System.out.println();
-          }
+        for (int i = 0; i < field.length; i++) {
+            int x = (int) (Math.random() * width);
+            field[i][x] = robotank;
+        }
+
+        for (int i = 0; i < bombs.length; i++) {
+            int bombCount = 10;
+            while (bombCount > 0) {
+                int j = (int) (Math.random() * width);
+                if (bombs[i][j] == 0) {
+                    bombs[i][j] = 1;
+                    bombCount--;
+                }
+            }
+        }
+
+        for (int i = 0; i < field.length; i++) {
+            for (int j = 0; j < field[i].length; j++) {
+                System.out.print(field[i][j]);
+            }
+            System.out.println();
+        }
     }
 }
