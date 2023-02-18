f6518dc41003ad91d2ab4888e78b336d7acfc679
macbook <babayev.nicat@yandex.ru>
2023-02-18 17:07:22 +0400

    add solution JavaSyntax task 1128

--- 1.JavaSyntax/src/com/javarush/task/pro/task11/task1128/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task11/task1128/Solution.java
@@ -20,24 +20,34 @@ public class Solution {
         switch (digit) {
             case '0':
                 result = "ноль";
+                break;
             case '1':
                 result = "один";
+                break;
             case '2':
                 result = "два";
+                break;
             case '3':
                 result = "три";
+                break;
             case '4':
                 result = "четыре";
+                break;
             case '5':
                 result = "пять";
+                break;
             case '6':
                 result = "шесть";
+                break;
             case '7':
                 result = "семь";
+                break;
             case '8':
                 result = "восемь";
+                break;
             case '9':
                 result = "девять";
+                break;
         }
         return result;
     }
