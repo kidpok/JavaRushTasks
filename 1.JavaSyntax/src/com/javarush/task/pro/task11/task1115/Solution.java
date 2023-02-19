461818616cc0bb8ec8c9eccf316a94eeb6410382
macbook <babayev.nicat@yandex.ru>
2023-02-20 02:37:45 +0400

    add solution JavaSyntax task 1115

--- 1.JavaSyntax/src/com/javarush/task/pro/task11/task1115/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task11/task1115/Solution.java
@@ -5,7 +5,7 @@ package com.javarush.task.pro.task11.task1115;
 */
 
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String... errors) {
         System.out.println("It works!");
     }
 }
