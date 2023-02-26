2a2ca7d829279e4072067d4fa70d0beae2c26b2a
macbook <babayev.nicat@yandex.ru>
2023-02-26 19:08:55 +0400

    add solution JavaSyntax task 1316

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task13/task1316/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.pro.task13.task1316;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        JavarushQuest[] javarushQuest = JavarushQuest.values();
+        for (JavarushQuest allQuest : javarushQuest) {
+            System.out.println(allQuest.ordinal());
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
