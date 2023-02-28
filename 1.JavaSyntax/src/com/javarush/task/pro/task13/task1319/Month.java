0cac9ab4988c5d5c49035ccb2b273fdbc2f378e0
macbook <babayev.nicat@yandex.ru>
2023-02-28 17:21:31 +0400

    add solution JavaSyntax task 1319

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task13/task1319/Month.java
@@ -0,0 +1,38 @@
+package com.javarush.task.pro.task13.task1319;
+
+/* 
+Месяцы в сезоне
+*/
+
+import java.util.Arrays;
+
+public enum Month {
+    JANUARY,
+    FEBRUARY,
+    MARCH,
+    APRIL,
+    MAY,
+    JUNE,
+    JULY,
+    AUGUST,
+    SEPTEMBER,
+    OCTOBER,
+    NOVEMBER,
+    DECEMBER;
+
+    public static Month[] getWinterMonths(){
+        return new Month[]{DECEMBER,JANUARY,FEBRUARY};
+    }
+    public static Month[] getSpringMonths(){
+        return new Month[]{MARCH,APRIL,MAY};
+    }
+    public static Month[] getSummerMonths(){
+        return new Month[]{JUNE,JULY,AUGUST};
+    }
+    public static Month[] getAutumnMonths(){
+        return Arrays.copyOfRange(Month.values(),8,11);
+    }
+
+
+
+}
