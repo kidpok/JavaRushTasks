de0f125c9c4c8d1b5c4b0b630e02759ab43e94e9
macbook <babayev.nicat@yandex.ru>
2023-04-04 06:04:45 -0600

    add solution JavaCore task1210

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1210/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task12.task1210;
+
+/* 
+Три метода и максимум
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static int max(int i, int a) {
+        if (i > a) {
+            return i;
+        } else return a;
+    }
+
+    public static long max(long i, long a) {
+        if (i > a) {
+            return i;
+        } else return a;
+    }
+
+    public static double max(double i, double a) {
+        if (i > a) {
+            return i;
+        } else return a;
+    }
+}
