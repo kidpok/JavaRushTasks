0b53fc19d9d088184692d3229a51a05585b82330
macbook <babayev.nicat@yandex.ru>
2023-04-07 06:30:01 -0600

    add solution JavaCore task1216

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1216/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task12.task1216;
+
+/* 
+Летать охота
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public interface CanFly {
+        static void getName() {
+
+        }
+
+        static void getHour() {
+
+        }
+    }
+
+}
