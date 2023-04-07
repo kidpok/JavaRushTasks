2669c7eb8aa890a0a334649fb185374c60dd2b5f
macbook <babayev.nicat@yandex.ru>
2023-04-07 06:32:04 -0600

    add solution JavaCore task1217

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1217/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task12.task1217;
+
+/* 
+Лететь, бежать и плыть
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public interface CanFly {
+    static void getFly(){
+
+    }
+
+    }
+    public interface CanRun{
+        static void getRun(){
+
+        }
+    }
+    public interface CanSwim{
+        static void getSwim(){
+
+        }
+    }
+
+}
