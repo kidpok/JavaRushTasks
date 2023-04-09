5bc60357a881bc75662fd3d983315f2e15e86f5d
macbook <babayev.nicat@yandex.ru>
2023-04-09 07:02:03 -0600

    add solution JavaCore task1220

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1220/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task12.task1220;
+
+/* 
+Класс Human и интерфейсы CanRun, CanSwim
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public abstract class Human implements CanRun, CanSwim {
+
+    }
+
+    public interface CanRun {
+        static void getRun() {
+
+        }
+    }
+
+    public interface CanSwim {
+        static void getSwim() {
+
+        }
+    }
+}
