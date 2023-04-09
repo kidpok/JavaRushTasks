57debbe021a8fad49da36e55496f62a672d61b74
macbook <babayev.nicat@yandex.ru>
2023-04-09 05:54:05 -0600

    add solution JavaCore task1207

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1207/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task12.task1207;
+
+/* 
+Int и Integer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Integer value = 4;
+        print(3);
+        print(value);
+    }
+
+    public static void print(int i){
+        System.out.println(i);
+    }
+    public static void print( Integer a){
+        System.out.println(a);
+    }
+
+
+
+}
