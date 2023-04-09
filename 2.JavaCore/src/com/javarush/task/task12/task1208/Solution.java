f3982bf2e48b24fa2bec14d202a88acc2ac762ef
macbook <babayev.nicat@yandex.ru>
2023-04-09 05:56:56 -0600

    add solution JavaCore task1208

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+   public static void print(int i){
+
+   }
+   public static void print(String a){
+
+   }
+   public static void print(String i, boolean a){
+
+   }
+   public static void print(int z, String a){
+
+   }
+   public static void print(int a, String g, boolean i){
+
+   }
+}
