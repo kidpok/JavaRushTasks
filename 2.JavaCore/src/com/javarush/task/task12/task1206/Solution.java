61a10ddb9171d0203dbd7eaf6e4f4eee52205df5
macbook <babayev.nicat@yandex.ru>
2023-04-09 05:39:15 -0600

    add solution JavaCore task1206

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1206/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task12.task1206;
+
+/* 
+Выполняем перегрузку!
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static int print(int i){
+
+        return i;
+    }
+    public static String print(String str){
+        return str;
+    }
+}
