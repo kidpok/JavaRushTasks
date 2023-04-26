dd252ff9b9290d98ae539ab35603a10cd790fa45
macbook <babayev.nicat@yandex.ru>
2023-04-26 00:06:08 -0400

    add solution JavaSyntax task1716

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task17/task1716/Vehicle.java
@@ -0,0 +1,15 @@
+package com.javarush.task.pro.task17.task1716;
+
+/* 
+Дорожное движение
+*/
+
+public interface Vehicle {
+    default void start(){
+
+    }
+    void move();
+   default void stop(){
+
+   }
+}
