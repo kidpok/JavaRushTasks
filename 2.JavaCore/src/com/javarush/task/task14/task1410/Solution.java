a7ed8d58561ed5624dd9cf4b7a25d8f85401de18
macbook <babayev.nicat@yandex.ru>
2023-05-04 21:45:29 -0400

    add solution JavaCore task1410

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1410/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task14.task1410;
+
+/* 
+Дегустация вин
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        getDeliciousDrink().taste();
+        System.out.println(getWine().getHolidayName());
+        System.out.println(getSparklingWine().getHolidayName());
+        System.out.println(getWine().getHolidayName());
+    }
+
+    public static Drink getDeliciousDrink() {
+       Wine wine = new Wine();
+       return wine;
+    }
+
+    public static Wine getWine() {
+    Wine wine = new Wine();
+    return wine;
+    }
+
+    public static Wine getSparklingWine() {
+        SparklingWine sparklingWine = new SparklingWine();
+        return sparklingWine;
+    }
+}
