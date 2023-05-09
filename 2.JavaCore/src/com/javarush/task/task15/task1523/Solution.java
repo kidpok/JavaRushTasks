2a4d058a2eea46ca3dedd535df80c858476143a4
macbook <babayev.nicat@yandex.ru>
2023-05-08 20:46:29 -0400

    add solution JavaCore task1523

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task15.task1523;
+
+/* 
+Перегрузка конструкторов
+*/
+
+public class Solution {
+    Solution() {
+    }
+
+    public Solution(double a) {
+    }
+
+    private Solution(int a) {
+    }
+
+    protected Solution(String n) {
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
+
---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/SubSolution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task15.task1523;
+
+public class SubSolution extends Solution{
+    protected SubSolution(int a){
+        super(a);
+
+    }
+
+    public SubSolution(Integer a) {
+        super(a);
+    }
+     SubSolution(String a) {
+        super(a);
+    }
+
+
+}
