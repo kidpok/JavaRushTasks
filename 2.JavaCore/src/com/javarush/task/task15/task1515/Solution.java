5760a7d7ae1bb3560778c75a58b7fbe9879b962b
macbook <babayev.nicat@yandex.ru>
2023-05-15 22:19:44 -0400

    add solution JavaCore task1515

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1515/Solution.java
@@ -0,0 +1,36 @@
+package com.javarush.task.task15.task1515;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+/* 
+Статики-2
+*/
+
+public class Solution {
+    public static int A;
+    public static int B;
+
+    static {
+        try{
+            Scanner console = new Scanner(System.in);
+            A = console.nextInt();
+            B = console.nextInt();
+        }catch (Exception exception){
+            System.out.println(exception);
+        }
+
+    }
+    public static final int MIN = min(A, B);
+
+    public static void main(String[] args) throws IOException {
+        System.out.println(MIN);
+    }
+
+    public static int min(int a, int b) {
+        return a < b ? a : b;
+    }
+
+}
