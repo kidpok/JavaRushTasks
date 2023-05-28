bfed927df11d7c724c740b08c6b93f07990e4595
macbook <babayev.nicat@yandex.ru>
2023-05-27 23:45:20 -0400

    add solution JavaCore task1704

---
+++ 2.JavaCore/src/com/javarush/task/task17/task1704/Solution.java
@@ -0,0 +1,35 @@
+package com.javarush.task.task17.task1704;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Синхронизированные заметки 2
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Note note = new Note();
+        note.start();
+    }
+
+    public static class Note extends Thread {
+
+        public final List<String> notes = new ArrayList<String>();
+
+        public synchronized void addNote(int index, String note) {
+            System.out.println("Сейчас будет добавлена заметка [" + note + "] На позицию " + index);
+            notes.add(index, note);
+            System.out.println("Уже добавлена заметка [" + note + "]");
+        }
+
+        public synchronized void  removeNote(int index) {
+            System.out.println("Сейчас будет удалена заметка с позиции " + index);
+            String note = notes.remove(index);
+            System.out.println("Уже удалена заметка [" + note + "] с позиции " + index);
+        }
+
+
+    }
+}
