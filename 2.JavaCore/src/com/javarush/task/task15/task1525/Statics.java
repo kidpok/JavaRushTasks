b889b16894cd5f1e281b7a6b7d33d459998d7cc7
macbook <babayev.nicat@yandex.ru>
2023-06-09 21:53:21 -0400

    add solution JavaCore task1525

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1525/Statics.java
@@ -0,0 +1,5 @@
+package com.javarush.task.task15.task1525;
+
+public class Statics {
+    public static String FILE_NAME = "/Users/macbook/Desktop/Java/JavaRushTasks/2.JavaCore/src/com/javarush/task/task15/task1525/Files";
+}
