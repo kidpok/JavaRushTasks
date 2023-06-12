04cff776a46df934ad391ff9a2da201ca97bd7e0
macbook <babayev.nicat@yandex.ru>
2023-06-12 00:22:24 -0400

    add solution JavaSyntax task0402

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0402/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.pro.task04.task0402;
+
+import java.util.Scanner;
+
+/* 
+Все любят Мамбу
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String text = " любит меня.";
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+
+        int a = 0;
+        while (a<10){
+            System.out.println(name + text);
+            a++;
+        }
+
+    }
+}
