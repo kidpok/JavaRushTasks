d0ab7fcf8c09711f875168fb15e315c01076b695
macbook <babayev.nicat@yandex.ru>
2023-06-12 00:20:21 -0400

    add solution JavaSyntax task0401

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0401/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task04.task0401;
+
+/* 
+Я никогда не буду работать за копейки
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String quote = "Я никогда не буду работать за копейки. Амиго";
+        int a = 0;
+        while (a <100){
+            System.out.println(quote);
+            a++;
+        }
+
+    }
+}
