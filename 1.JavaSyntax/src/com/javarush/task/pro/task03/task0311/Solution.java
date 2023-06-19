3923cb9e5d8993d41dc61cba8824c756a7e37498
macbook <babayev.nicat@yandex.ru>
2023-06-18 23:51:54 -0400

    add solution JavaSyntax task0311

--- 1.JavaSyntax/src/com/javarush/task/pro/task03/task0311/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0311/Solution.java
@@ -8,15 +8,15 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
-        Scanner console = new Scanner(System.in);
-        double number1 = console.nextDouble();
-        double number2 = console.nextDouble();
-        if ( Math.abs(number2 - number1)<.000001){
+        //напишите тут ваш код
+        Scanner scanner = new Scanner(System.in);
+        double a = scanner.nextDouble();
+        double b = scanner.nextDouble();
+
+        if (Math.abs(b - a) < 0.000001)
             System.out.println("числа равны");
-        }else {
+        else {
             System.out.println("числа не равны");
         }
-
-
     }
 }
