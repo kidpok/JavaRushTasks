a127d519dc6cc51ff32c8fa64f57a03d8c9cce92
macbook <babayev.nicat@yandex.ru>
2023-07-10 10:04:06 -0400

    add solution JavaSyntax task0206

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0206/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task02.task0206;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String word = "a";
+        String phrase = "as";
+        String line = "asa";
+        String text = "asas";
+
+
+    }
+}
