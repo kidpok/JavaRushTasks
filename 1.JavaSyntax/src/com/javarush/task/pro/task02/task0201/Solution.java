4dfdb5f588e09cfb3c87fc23f57fb3e56e985462
macbook <babayev.nicat@yandex.ru>
2023-07-10 09:57:20 -0400

    add solution JavaSyntax task0105

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0201/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.pro.task02.task0201;
+
+/* 
+«Я по объявлению…»
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int intValue = 2;
+        int numericValue = 3;
+        int size = 5;
+        int number = 6;
+
+
+
+    }
+}
