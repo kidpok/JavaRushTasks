7765a7cba31d38a7bfcf1ed34f3ea6023bc18cbd
macbook <babayev.nicat@yandex.ru>
2023-07-12 00:55:18 -0400

    add solution JavaSyntax task0404

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0404/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.pro.task04.task0404;
+
+/* 
+Заполненный прямоугольник
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+        int a = 0;
+        while (a<5){
+            int b = 0;
+           while (b<9){
+                System.out.print("Q");
+               b++;
+            }
+            System.out.println("Q");
+            a++;
+        }
+
+    }
+}
