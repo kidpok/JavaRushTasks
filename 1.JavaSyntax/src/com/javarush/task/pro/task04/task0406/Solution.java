018b422ec66b91c2bd5fb2bb0e4a79d81a55bb12
macbook <babayev.nicat@yandex.ru>
2023-07-13 23:35:09 -0400

    add solution JavaSyntax task0406

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0406/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0406/Solution.java
@@ -8,7 +8,14 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
+       Scanner scanner = new Scanner(System.in);
 
+       while (true) {
+           String str = scanner.nextLine();
+           if ( str.equals("enough")){
+               break;
+           }
+           System.out.println(str);
+       }
     }
 }
