ec69bbed9f5d1f5d5c3e0d6cb5f5b1e6d4d0b5e0
macbook <babayev.nicat@yandex.ru>
2023-07-31 00:00:00 -0400

    add solution JavaSyntax task0613

--- 1.JavaSyntax/src/com/javarush/task/pro/task06/task0613/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0613/Solution.java
@@ -15,7 +15,7 @@ public class Solution {
         printCityPopulation("Нью-Йорк", 21.6);
     }
 
-    public static void printCityPopulation(String city, double population){
+    private static void printCityPopulation(String city, double population){
         System.out.println("Население города " + city + " составляет " + population + " млн человек.");
         System.out.println("В то время как в самом густонаселенном городе " + Solution.city + " население составляет " + Solution.population + " млн человек.");
     }
