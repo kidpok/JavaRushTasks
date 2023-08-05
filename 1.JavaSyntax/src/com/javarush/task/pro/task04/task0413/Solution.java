69841c272074fffbf6d09a6276c7310f756109f9
macbook <babayev.nicat@yandex.ru>
2023-08-04 22:08:44 -0400

    add solution JavaSyntax task0413

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0413/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.pro.task04.task0413;
+
+/* 
+Рисуем треугольник
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int x = 0;
+        for (int i = 0; i < 10; i++) {
+            int y = 0;
+            for (int j = 0; j <= i; j++) {
+                    System.out.print("8");
+            }
+            System.out.println();
+        }
+
+    }
+}
