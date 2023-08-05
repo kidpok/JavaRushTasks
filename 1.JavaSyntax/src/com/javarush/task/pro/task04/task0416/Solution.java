ad6d8ae6bd2aad56eeab12878a5e253b804e06af
macbook <babayev.nicat@yandex.ru>
2023-08-04 22:42:13 -0400

    add solution JavaSyntax task0416

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0416/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.pro.task04.task0416;
+
+import java.util.Scanner;
+
+/* 
+Share a Coke
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        float result = (float) num1 / num2;
+        System.out.println(result);
+
+    }
+}
