de0878f9fb8e5c98a0e98fde1b22a5bc0220ae55
macbook <babayev.nicat@yandex.ru>
2023-08-05 20:45:51 -0400

    add solution JavaSyntax task0602

--- 1.JavaSyntax/src/com/javarush/task/pro/task06/task0602/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0602/Solution.java
@@ -6,11 +6,8 @@ package com.javarush.task.pro.task06.task0602;
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
         System.out.println("Я — поэт, зовусь я Цветик.");
         System.out.println("От меня вам всем приветик.");
-
     }
-
 
 }
