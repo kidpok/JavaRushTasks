550cd7edad9a3b01004ad45da46a062cf5238283
macbook <babayev.nicat@yandex.ru>
2023-08-05 22:01:35 -0400

    add solution JavaSyntax task0505

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0505/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0505/Solution.java
@@ -1,6 +1,5 @@
 package com.javarush.task.pro.task05.task0505;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 /* 
@@ -9,27 +8,25 @@ Reverse
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
-        Scanner console = new Scanner(System.in);
-        int number = console.nextInt();
-        int[] a = new int[number];
-        for (int i = 0; i < a.length; i++) {
-            if (number > 0) {
-                a[i] = console.nextInt();
-            } else break;
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        int[] array = new int[n];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = scanner.nextInt();
         }
-        if (number % 2 == 0) {
-            for (int i = a.length - 1; i >= 0; i--) {
-                System.out.println(a[i]);
+
+        if (n % 2 == 0) {
+            for (int i = array.length-1 ; i >= 0; i--) {
+                System.out.println(array[i]);
             }
         } else {
-            for (int i = 0; i < a.length; i++) {
-                System.out.println(a[i]);
+            for (int i = 0; i < array.length; i++) {
+                System.out.println(array[i]);
             }
         }
     }
 }
-
-
 
 
