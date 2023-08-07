55f64f9dd98821cdaa9b2b6901e682f42c886fc9
macbook <babayev.nicat@yandex.ru>
2023-08-07 00:05:46 -0400

    add solution JavaSyntax task0614

--- 1.JavaSyntax/src/com/javarush/task/pro/task06/task0614/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0614/Solution.java
@@ -9,7 +9,7 @@ public class Solution {
     public static double radius = 2.5;
     public static double diameter = 5;
 
-    public static  void main(String[] args) {
+    public static void main(String[] args) {
         System.out.println("Плошадь круга равна: " + PI * radius * radius);
         System.out.println("Периметр круга равен: " + PI * diameter);
     }
