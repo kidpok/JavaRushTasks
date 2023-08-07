117abbc51253233cead98a222fc93a111d48ece5
macbook <babayev.nicat@yandex.ru>
2023-08-07 00:09:37 -0400

    add solution JavaSyntax task0615

--- 1.JavaSyntax/src/com/javarush/task/pro/task06/task0615/Earth.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0615/Earth.java
@@ -6,7 +6,7 @@ package com.javarush.task.pro.task06.task0615;
 
 public class Earth {
     public static final String NAME = "Земля";
-     public static final double SQUARE = 510_100_000;
+    public static final double SQUARE = 510_100_000;
     public static final long POPULATION = 7_594_000_000L;
     public static final long EQUATOR_LENGTH = 40_075_696;
 }
