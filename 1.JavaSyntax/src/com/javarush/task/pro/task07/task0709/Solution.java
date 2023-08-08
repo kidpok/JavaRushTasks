3a720219c3a2e335356510acdcea8455d7af4c57
macbook <babayev.nicat@yandex.ru>
2023-08-07 21:51:41 -0400

    add solution JavaSyntax task0709

--- 1.JavaSyntax/src/com/javarush/task/pro/task07/task0709/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0709/Solution.java
@@ -5,8 +5,8 @@ package com.javarush.task.pro.task07.task0709;
 */
 
 public class Solution {
-    long a = (long) (109 + 15);
-    int b = (int) (a * 2);
+    long a = 109 + 15;
+    int b = (int) a * 2;
     short c = (short) (a / b);
     byte d = (byte) (a + b - c);
 }
