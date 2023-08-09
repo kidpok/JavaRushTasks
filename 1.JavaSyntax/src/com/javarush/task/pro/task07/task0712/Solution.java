8eede7f8364939bd733c844a38b7910d85a6884d
macbook <babayev.nicat@yandex.ru>
2023-08-08 22:32:37 -0400

    add solution JavaSyntax task0712

--- 1.JavaSyntax/src/com/javarush/task/pro/task07/task0712/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0712/Solution.java
@@ -8,24 +8,24 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
-        String string = new String();
-        String string1 = new String();
-        String string2 = new String();
-        String string3 = new String();
-        String string4 = new String();
-        String string5 = new String();
-        String string6 = new String();
-        String string7 = new String();
-        String string8 = new String();
-        String string9 = new String();
-       int[] ints = new int[1];
-        int[] ints1 = new int[2];
-        int[] ints2 = new int[3];
-        int[] ints3 = new int[4];
-        int[] ints4 = new int[5];
-        Scanner console = new Scanner(System.in);
-        Scanner console1 = new Scanner(System.in);
+       Scanner scanner = new Scanner(System.in);
+       Scanner scanner1 = new Scanner(System.in);
+       new String();
+        new String();
+        new String();
+        new String();
+        new String();
+        new String();
+        new String();
+        new String();
+        new String();
+        new String();
+
+        int[] a = new int[1];
+        int[] b = new int[2];
+        int[] c = new int[3];
+        int[] d = new int[4];
+        int[] e = new int[5];
 
 
 
