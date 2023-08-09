6bc3ae2cba269a4ea5aed081cce6b40f47b8d4b3
macbook <babayev.nicat@yandex.ru>
2023-08-08 22:43:57 -0400

    add solution JavaSyntax task0715

--- 1.JavaSyntax/src/com/javarush/task/pro/task07/task0715/JavaDeveloper.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0715/JavaDeveloper.java
@@ -1,6 +1,6 @@
 package com.javarush.task.pro.task07.task0715;
 
-public class JavaDeveloper extends Human{
+public class JavaDeveloper  extends Human{
 
     public void code(){
         System.out.println("Я умею общаться на Java.");
