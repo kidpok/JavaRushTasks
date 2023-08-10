3832baf6312ed43005baa3fab53b792cf5fad9ee
macbook <babayev.nicat@yandex.ru>
2023-08-09 20:38:31 -0400

    add solution JavaSyntax task0802

--- 1.JavaSyntax/src/com/javarush/task/pro/task08/task0802/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task08/task0802/Solution.java
@@ -15,15 +15,6 @@ public class Solution {
     }
 
     public static double pow(int number, int power) {
-        if (power == 0) {
-            return Math.pow(number, power);
-        }
-
-        int modulus = power < 0 ? power * -1 : power;
-        int result = number;
-        for (int i = 1; i < modulus; i++) {
-            result *= number;
-        }
-        return power < 0 ? 1.0 / result : result;
+        return Math.pow(number,power);
     }
 }
