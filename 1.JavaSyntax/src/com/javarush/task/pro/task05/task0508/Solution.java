d0bd8528df9e9a588e9ca14c4e43f9d2bc616edf
macbook <babayev.nicat@yandex.ru>
2023-08-10 23:19:25 -0400

    add solution JavaSyntax task0508

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0508/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0508/Solution.java
@@ -10,29 +10,28 @@ public class Solution {
     public static String[] strings;
 
     public static void main(String[] args) {
-        //напишите тут ваш код
-        Scanner console = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+
         strings = new String[6];
-        for (int i = 0; i < strings.length; i++) {
-            strings[i] = console.nextLine();
+        for (int i = 0; i < 6; i++) {
+            strings[i] = scanner.nextLine();
         }
 
-        for (int i = 0; i < strings.length - 1; i++) {
-            String tmp = strings[i];
+        for (int i = 0; i < strings.length; i++) {
+            String currentString = strings[i];
             for (int j = i + 1; j < strings.length; j++) {
-                if (tmp == null ){
-                    break;
+                if (currentString == null) {
+                    continue;
                 }
-                if (tmp.equals(strings[j])) {
-                    strings[i] = null;
+                if (currentString.equals(strings[j])) {
                     strings[j] = null;
+                    strings[i] = null;
                 }
             }
         }
-
-
         for (int i = 0; i < strings.length; i++) {
             System.out.print(strings[i] + ", ");
         }
     }
 }
+
