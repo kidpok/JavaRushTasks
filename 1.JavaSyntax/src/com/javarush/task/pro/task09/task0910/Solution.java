5997559beb035fe38c737b78bdf88bf11b465fe8
macbook <babayev.nicat@yandex.ru>
2023-08-14 00:08:45 -0400

    add solution JavaSyntax task0910

--- 1.JavaSyntax/src/com/javarush/task/pro/task09/task0910/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task09/task0910/Solution.java
@@ -4,8 +4,6 @@ package com.javarush.task.pro.task09.task0910;
 Кодировка Unicode
 */
 
-import com.sun.security.auth.UnixNumericGroupPrincipal;
-
 public class Solution {
     public static void main(String[] args) {
         char[] symbols = new char[9];
@@ -14,15 +12,17 @@ public class Solution {
     }
 
     public static void init(char[] symbols) {
-        symbols[0] = '\u00A9';
-        symbols[1] = '\u004A';
-        symbols[2] = '\u0061';
-        symbols[3] = '\u0076';
-        symbols[4] = '\u0061';
-        symbols[5] = '\u0052';
-        symbols[6] = '\u0075';
-        symbols[7] = '\u0073';
-        symbols[8] = '\u0068';
+        for (int i = 0; i < symbols.length; i++) {
+            symbols[0] = '\u00A9';
+            symbols[1] = '\u004A';
+            symbols[2] = '\u0061';
+            symbols[3] = '\u0076';
+            symbols[4] = '\u0061';
+            symbols[5] = '\u0052';
+            symbols[6] = '\u0075';
+            symbols[7] = '\u0073';
+            symbols[8] = '\u0068';
 
+        }
     }
 }
