89c9796e46965840d2251bfb6844cc6ab22049b3
macbook <babayev.nicat@yandex.ru>
2023-08-19 21:37:17 -0400

    add solution JavaSyntax task1002

--- 1.JavaSyntax/src/com/javarush/task/pro/task10/task1002/Skyscraper.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1002/Skyscraper.java
@@ -9,21 +9,22 @@ public class Skyscraper {
     public static final String SKYSCRAPER_WAS_BUILD_FLOORS_COUNT = "Небоскреб построен. Количество этажей - ";
     public static final String SKYSCRAPER_WAS_BUILD_DEVELOPER = "Небоскреб построен. Застройщик - ";
 
-    public Skyscraper(){
-        System.out.println("Небоскреб построен.");
+    public Skyscraper(int a) {
+        System.out.println(SKYSCRAPER_WAS_BUILD_FLOORS_COUNT + a);
     }
-    public Skyscraper(int x){
-        System.out.println("Небоскреб построен. Количество этажей - " + x);
+
+    public Skyscraper(String a) {
+        System.out.println(SKYSCRAPER_WAS_BUILD_DEVELOPER + a);
     }
-    public Skyscraper(String j){
-        System.out.println("Небоскреб построен. Застройщик - " + j);
+
+    public Skyscraper() {
+        System.out.println(SKYSCRAPER_WAS_BUILD);
     }
 
     public static void main(String[] args) {
         Skyscraper skyscraper = new Skyscraper();
         Skyscraper skyscraperTower = new Skyscraper(50);
         Skyscraper skyscraperSkyline = new Skyscraper("JavaRushDevelopment");
-        System.out.println();
     }
 }
 
