4aa343745d8e226a05af7153c70104a2822bb70c
macbook <babayev.nicat@yandex.ru>
2023-08-19 21:32:48 -0400

    add solution JavaSyntax task1001

--- 1.JavaSyntax/src/com/javarush/task/pro/task10/task1001/Skyscraper.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1001/Skyscraper.java
@@ -5,13 +5,14 @@ package com.javarush.task.pro.task10.task1001;
 */
 
 public class Skyscraper {
-      public Skyscraper(){
-          System.out.println("Небоскреб построен.");
-      }
 
+    public Skyscraper(){
+        System.out.println("Небоскреб построен.");
+    }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {;
         Skyscraper skyscraper = new Skyscraper();
-        
+
+
     }
 }
