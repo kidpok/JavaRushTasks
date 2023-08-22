b0d479a707148803fcd87aff76270c90ae2a5fd0
macbook <babayev.nicat@yandex.ru>
2023-08-21 21:02:24 -0400

    add solution JavaSyntax task1007

--- 1.JavaSyntax/src/com/javarush/task/pro/task10/task1007/Bugatti.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1007/Bugatti.java
@@ -9,15 +9,31 @@ public class Bugatti {
     private int year = 2020;
     private String body = "Coupe";
 
-    public Bugatti(){
+    public Bugatti() {
 
     }
 
-     public String getBody(){
+    public String getColor() {
+        return color;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getBody() {
         return body;
-     }
-     public void setBody(String body){
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setBody(String body) {
         this.body = body;
-     }
-    //напишите тут ваш код
+    }
 }
