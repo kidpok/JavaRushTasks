f35bb843cf1a22df8e532a2ee0ab612d1239fd5a
macbook <babayev.nicat@yandex.ru>
2023-08-21 21:00:49 -0400

    add solution JavaSyntax task1006

--- 1.JavaSyntax/src/com/javarush/task/pro/task10/task1006/ElectricCar.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1006/ElectricCar.java
@@ -3,6 +3,6 @@ package com.javarush.task.pro.task10.task1006;
 public class ElectricCar extends Car {
 
     public ElectricCar() {
-        super("ElectricCar");
+       super("ElectricCar");
     }
  }
--- 1.JavaSyntax/src/com/javarush/task/pro/task10/task1006/HybridCar.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1006/HybridCar.java
@@ -4,6 +4,5 @@ public class HybridCar extends Car {
 
     public HybridCar() {
         super("HybridCar");
-
     }
 }
