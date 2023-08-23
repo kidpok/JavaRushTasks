743bf2c0e6ccceb0908fec3d8dd6ce087ced0288
macbook <babayev.nicat@yandex.ru>
2023-08-22 21:03:50 -0400

    add solution JavaSyntax task1008

--- 1.JavaSyntax/src/com/javarush/task/pro/task10/task1008/Programmer.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1008/Programmer.java
@@ -7,17 +7,13 @@ package com.javarush.task.pro.task10.task1008;
 public class Programmer {
     private int salary = 1000;
 
-    public int getSalary(){
+    public int getSalary() {
         return salary;
     }
 
-    public void setSalary(int salary){
+    public void setSalary(int salary) {
         if (salary > this.salary){
             this.salary = salary;
-        }else {
-
         }
     }
-
-
 }
