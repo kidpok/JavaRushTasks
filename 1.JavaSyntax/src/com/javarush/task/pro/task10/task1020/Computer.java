e01841c928b6e3d5d7222f079ff40622dd426428
macbook <babayev.nicat@yandex.ru>
2023-08-25 22:11:54 -0400

    add solution JavaSyntax task1020

--- 1.JavaSyntax/src/com/javarush/task/pro/task10/task1020/Computer.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1020/Computer.java
@@ -1,8 +1,9 @@
 package com.javarush.task.pro.task10.task1020;
 
 public class Computer {
-  Keyboard keyboard = new Keyboard();
-  Monitor monitor = new Monitor();
-  Mouse mouse = new Mouse();
-  SystemUnit systemUnit = new SystemUnit();
+        Monitor monitor = new Monitor();
+        Mouse mouse = new Mouse();
+        SystemUnit systemUnit = new SystemUnit();
+        Keyboard keyboard = new Keyboard();
+
 }
