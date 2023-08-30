7f7c36748b131584456da2bbbaf5e0d5a47a9f04
macbook <babayev.nicat@yandex.ru>
2023-08-29 22:02:08 -0400

    add solution JavaSyntax task1102

--- 1.JavaSyntax/src/com/javarush/task/pro/task11/task1102/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task11/task1102/Solution.java
@@ -8,7 +8,6 @@ public class Solution {
 
     public static void main(String[] args) {
         Planet earth = new Planet();
-
         earth.name = "Земля";
         earth.age = 4_540_000_000L;
         earth.speed = 170_218;
