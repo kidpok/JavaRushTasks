b8f9505bbff3c1c18b6f29468fe5a6045113ffe3
macbook <babayev.nicat@yandex.ru>
2023-08-30 22:23:00 -0400

    add solution JavaSyntax task1104

--- 1.JavaSyntax/src/com/javarush/task/pro/task11/task1104/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task11/task1104/Solution.java
@@ -7,8 +7,8 @@ package com.javarush.task.pro.task11.task1104;
 public class Solution {
     public static void main(String[] args) {
         String string = "12.84";
-        double dob = Double.parseDouble(string);
-        int number = (int) Math.round(dob);
-        System.out.println(number);
+        double v = Double.parseDouble(string);
+        long round = Math.round(v);
+        System.out.println(round);
     }
 }
