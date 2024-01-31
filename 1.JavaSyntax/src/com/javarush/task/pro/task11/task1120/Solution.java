ed3ed5a2aea16139d91d2526e6a68e9f1170f0a5
macbook <babayev.nicat@yandex.ru>
2024-01-30 19:20:59 -0500

    add solution JavaSyntax task1120

--- 1.JavaSyntax/src/com/javarush/task/pro/task11/task1120/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task11/task1120/Solution.java
@@ -10,7 +10,7 @@ public class Solution {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
         String line = console.nextLine();
-        String str = line.toLowerCase();
-        System.out.println(str);
+        String lowerCase = line.toLowerCase();
+        System.out.println(lowerCase);
     }
 }
