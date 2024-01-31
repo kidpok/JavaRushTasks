ef97ea7423442fa16d054349c2b37df2d2693949
macbook <babayev.nicat@yandex.ru>
2024-01-30 19:15:21 -0500

    add solution JavaSyntax task1110

--- 1.JavaSyntax/src/com/javarush/task/pro/task11/task1110/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task11/task1110/Solution.java
@@ -6,7 +6,7 @@ package com.javarush.task.pro.task11.task1110;
 
 public class Solution {
     public static void main(String[] args) {
-        int number = 1;
+        int number = 1 ;
         System.out.println(number);
     }
 }
