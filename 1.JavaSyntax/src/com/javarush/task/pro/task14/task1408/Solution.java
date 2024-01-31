d4e28712fcef883500f27e264441286079804128
macbook <babayev.nicat@yandex.ru>
2024-01-31 11:32:11 -0500

    add solution JavaSyntax task1408

--- 1.JavaSyntax/src/com/javarush/task/pro/task14/task1408/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task14/task1408/Solution.java
@@ -2,26 +2,22 @@ package com.javarush.task.pro.task14.task1408;
 
 /* 
 Проглатываем исключение
-*/
+
 
 public class Solution {
 
     public static String errorMessage = "не повезло";
 
     public static void main(String[] args) {
-        try{
-            generateLuckyNumber();
-        }catch (Exception exp){
-            System.out.println(errorMessage);
-        }
-
+        generateLuckyNumber();
     }
 
     static void generateLuckyNumber() throws Exception {
-        int luckyNumber =   (int) (Math.random() * 100);
+        int luckyNumber = (int) (Math.random() * 100);
         if (luckyNumber == 13) {
             throw new Exception();
         }
         System.out.println("твое счастливое число: " + luckyNumber);
     }
 }
+*/
