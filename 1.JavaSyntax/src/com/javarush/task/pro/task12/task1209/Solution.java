0cad3a7713e6dccdc112e41177eee4b1ef68e4ac
macbook <babayev.nicat@yandex.ru>
2024-02-02 22:11:09 -0500

    add solution JavaSyntax task1209

--- 1.JavaSyntax/src/com/javarush/task/pro/task12/task1209/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task12/task1209/Solution.java
@@ -25,11 +25,9 @@ public class Solution {
     }
 
     public static void paySalary(String name) {
-       if ( waitingEmployees.contains(name)){
-           alreadyGotSalaryEmployees.add(name);
-           waitingEmployees.set(waitingEmployees.indexOf(name), null);
-       }
-
-
+        if (waitingEmployees.contains(name)) {
+            alreadyGotSalaryEmployees.add(name);
+            waitingEmployees.set(waitingEmployees.indexOf(name), null);
+        }
     }
 }
