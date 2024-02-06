fd253c93670e7ff829e9fd7d15d89bd84f9bf5d7
macbook <babayev.nicat@yandex.ru>
2024-02-05 22:49:55 -0500

    add solution JavaSyntax task1302

--- 1.JavaSyntax/src/com/javarush/task/pro/task13/task1302/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task13/task1302/Solution.java
@@ -13,9 +13,8 @@ public class Solution {
     public static HashSet<String> words = new HashSet<>(asList("Если бы меня попросили выбрать язык на замену Java я бы не выбирал".split(" ")));
 
     public static void checkWords(String word) {
-        if(words.contains(word)) {
-
-            System.out.println("Слово " + word + " есть в множестве");
+        if( words.contains(word)){
+            System.out.println("Слово " + word + " есть в множестве" );
         }else {
             System.out.println("Слова " + word + " нет в множестве");
         }
