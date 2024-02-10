facda5933d9b33cc6c3713c48409c0c526b4f184
macbook <babayev.nicat@yandex.ru>
2024-02-09 23:01:41 -0500

    add solution JavaSyntax task1410

--- 1.JavaSyntax/src/com/javarush/task/pro/task14/task1410/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task14/task1410/Solution.java
@@ -9,21 +9,18 @@ import java.nio.file.FileSystemException;
 
 public class Solution {
 
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
         copyFile("book.txt", "book_final_copy.txt");
         copyFile("book_final_copy.txt", "book_last_copy.txt");
     }
 
-    static void copyFile(String sourceFile, String destinationFile)  {
-        try {
-            FileUtils.readFile(sourceFile);
-        }catch (FileNotFoundException e){
-            throw new RuntimeException(e);
+    static void copyFile(String sourceFile, String destinationFile) {
+        try{
+        FileUtils.readFile(sourceFile);
+        FileUtils.writeFile(destinationFile);
+        }catch (Exception e){
+           throw new RuntimeException(e);
         }
-        try {
-            FileUtils.writeFile(destinationFile);
-        }catch (FileSystemException e){
-            throw new RuntimeException(e);
-        }
+
     }
 }
