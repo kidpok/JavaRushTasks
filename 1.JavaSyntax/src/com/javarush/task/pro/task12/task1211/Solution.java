4b00688730b61e13df8bec1fb0c3b09653e657b3
macbook <babayev.nicat@yandex.ru>
2024-02-11 22:27:18 -0500

    add solution JavaSyntax task1211

--- 1.JavaSyntax/src/com/javarush/task/pro/task12/task1211/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task12/task1211/Solution.java
@@ -9,7 +9,7 @@ import java.util.Arrays;
 
 public class Solution {
 
-    public static int[] numbers = new int[] {56, 45, 89, 1, 45, 13, 4, 69, 11, -89};
+    public static int[] numbers = new int[]{56, 45, 89, 1, 45, 13, 4, 69, 11, -89};
 
     public static void main(String[] args) {
         print();
@@ -24,7 +24,7 @@ public class Solution {
                 if (numbers[j] > numbers[j + 1]) {
                     int temp = numbers[j];
                     numbers[j] = numbers[j + 1];
-                    numbers[j + 1] = temp;
+                     numbers[j + 1] = temp;
                 }
             }
         }
