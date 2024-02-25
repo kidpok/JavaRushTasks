a7404adc689075297549a09b49434f3cd22f7340
macbook <babayev.nicat@yandex.ru>
2024-02-25 11:18:50 -0500

    add solution JavaSyntax task1707

--- 1.JavaSyntax/src/com/javarush/task/pro/task17/task1707/Dog.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task17/task1707/Dog.java
@@ -3,6 +3,7 @@ package com.javarush.task.pro.task17.task1707;
 public class Dog extends Pet {
     public static final String DOG = "Я люблю людей.";
 
+    @Override
     public void printInfo() {
         super.printInfo();
         System.out.println(DOG);
