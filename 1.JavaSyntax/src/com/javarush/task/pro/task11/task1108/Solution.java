8a660a4e090cc005ca59aba5943be80eeb8b5c6a
macbook <babayev.nicat@yandex.ru>
2024-02-26 21:47:51 -0500

    add solution JavaSyntax task1108

--- 1.JavaSyntax/src/com/javarush/task/pro/task11/task1108/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task11/task1108/Solution.java
@@ -7,7 +7,17 @@ package com.javarush.task.pro.task11.task1108;
 public class Solution {
     public static final String EQUAL = " = ";
 
-    public static class Calculator{
+    public static void main(String[] args) {
+        int a = 45;
+        int b = 15;
+        Calculator.add(a, b);
+        Calculator.subtract(a, b);
+        Calculator.multiply(a, b);
+        Calculator.divide(a, b);
+    }
+
+
+    static class Calculator{
         public static void add(int a, int b) {
             System.out.println(a + " + " + b + EQUAL + (a + b));
         }
@@ -24,14 +34,5 @@ public class Solution {
             System.out.println(a + " / " + b + EQUAL + (a / b));
         }
     }
-    public static void main(String[] args) {
-        int a = 45;
-        int b = 15;
-        Calculator.add(a, b);
-        Calculator.subtract(a, b);
-        Calculator.multiply(a, b);
-        Calculator.divide(a, b);
-    }
-
 
 }
