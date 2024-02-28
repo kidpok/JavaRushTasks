72dbf15819b979e369862e4a90ba7759eb4396cc
macbook <babayev.nicat@yandex.ru>
2024-02-27 21:21:25 -0500

    add solution JavaSyntax task1502

--- 1.JavaSyntax/src/com/javarush/task/pro/task15/task1502/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1502/Solution.java
@@ -8,13 +8,14 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        try  {
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(System.in);
             String line = scanner.nextLine();
             System.out.println(line.toUpperCase());
         } catch (Exception e) {
             System.out.println("Something went wrong : " + e);
-        }finally {
+        } finally {
             scanner.close();
         }
     }
