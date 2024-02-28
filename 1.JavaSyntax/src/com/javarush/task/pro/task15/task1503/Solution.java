e1533e105100df11d860642f30a6b357fc74dd8a
macbook <babayev.nicat@yandex.ru>
2024-02-27 21:24:13 -0500

    add solution JavaSyntax task1503

--- 1.JavaSyntax/src/com/javarush/task/pro/task15/task1503/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1503/Solution.java
@@ -13,15 +13,15 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) throws IOException {
 
-        try ( Scanner scanner = new Scanner(System.in); BufferedReader bufferedReader = Files.newBufferedReader(Path.of(scanner.nextLine()))){
 
+        try (Scanner scanner = new Scanner(System.in); BufferedReader bufferedReader = Files.newBufferedReader(Path.of(scanner.nextLine()))) {
             String line;
             while ((line = bufferedReader.readLine()) != null) {
                 System.out.println(line);
             }
-        } catch (IOException e) {
+        } catch (
+                IOException e) {
             System.out.println("Something went wrong : " + e);
-
         }
     }
 }
