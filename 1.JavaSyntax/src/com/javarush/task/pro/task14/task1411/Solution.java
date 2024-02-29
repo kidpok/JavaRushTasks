59f216a1dd1e2893c816882ccab925e33066fdd4
macbook <babayev.nicat@yandex.ru>
2024-02-28 22:36:12 -0500

    add solution JavaSyntax task1411

--- 1.JavaSyntax/src/com/javarush/task/pro/task14/task1411/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task14/task1411/Solution.java
@@ -15,19 +15,18 @@ public class Solution {
     public static FileManager fileManager = new FileManager();
 
     public static void main(String[] args) {
-
         try {
             fileManager.copyFile("book.txt", "book_final_copy.txt");
             fileManager.copyFile("book_final_copy.txt", "book_last_copy.txt");
-        } catch (RuntimeException r) {
-            Throwable cause = r.getCause();
+        } catch (RuntimeException e) {
+            Throwable cause = e.getCause();
             if (cause instanceof FileNotFoundException) {
                 System.out.println(FAILED_TO_READ);
             } else if (cause instanceof FileSystemException) {
                 System.out.println(FAILED_TO_WRITE);
+
             }
         }
-
 
     }
 }
