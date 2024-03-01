8fc590756a38fdf23a3eaf8aed26861b5629bf66
macbook <babayev.nicat@yandex.ru>
2024-02-29 22:44:29 -0500

    add solution JavaSyntax task1219

--- 1.JavaSyntax/src/com/javarush/task/pro/task12/task1219/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task12/task1219/Solution.java
@@ -9,7 +9,8 @@ import java.util.ArrayList;
 public class Solution {
 
     public static void main(String[] args) {
-        var strings = new ArrayList<String>() {{
+        var strings = new ArrayList<String>()
+        {{
                 add("Так");
                 add("тоже");
                 add("можно");
