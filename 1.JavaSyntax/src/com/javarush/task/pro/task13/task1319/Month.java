146024f45b95fead82fac998a5a1bd9182f4c8f5
macbook <babayev.nicat@yandex.ru>
2024-03-01 21:11:41 -0500

    add solution JavaSyntax task1319

--- 1.JavaSyntax/src/com/javarush/task/pro/task13/task1319/Month.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task13/task1319/Month.java
@@ -4,8 +4,6 @@ package com.javarush.task.pro.task13.task1319;
 Месяцы в сезоне
 */
 
-import java.util.Arrays;
-
 public enum Month {
     JANUARY,
     FEBRUARY,
@@ -20,19 +18,38 @@ public enum Month {
     NOVEMBER,
     DECEMBER;
 
-    public static Month[] getWinterMonths(){
-        return new Month[]{DECEMBER,JANUARY,FEBRUARY};
-    }
-    public static Month[] getSpringMonths(){
-        return new Month[]{MARCH,APRIL,MAY};
-    }
-    public static Month[] getSummerMonths(){
-        return new Month[]{JUNE,JULY,AUGUST};
-    }
-    public static Month[] getAutumnMonths(){
-        return Arrays.copyOfRange(Month.values(),8,11);
+    public static Month[] getWinterMonths() {
+        Month[] winter = new Month[3];
+        winter[0] = Month.DECEMBER;
+        winter[1] = Month.JANUARY;
+        winter[2] = Month.FEBRUARY;
+        return winter;
+
     }
 
+    public static Month[] getSpringMonths() {
+        Month[] spring = new Month[3];
+        spring[0] = Month.MARCH;
+        spring[1] = Month.APRIL;
+        spring[2] = Month.MAY;
+        return spring;
 
+    }
+
+    public static Month[] getSummerMonths() {
+        Month[] summer = new Month[3];
+        summer[0] = Month.JUNE;
+        summer[1] = Month.JULY;
+        summer[2] = Month.AUGUST;
+        return summer;
+    }
+
+    public static Month[] getAutumnMonths() {
+        Month[] autumn = new Month[3];
+        autumn[0] = Month.SEPTEMBER;
+        autumn[1] = Month.OCTOBER;
+        autumn[2] = Month.NOVEMBER;
+        return autumn;
+    }
 
 }
