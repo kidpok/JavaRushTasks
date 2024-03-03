c53c31b692a0cfed5a95a1f55ff710f9889a56e3
macbook <babayev.nicat@yandex.ru>
2024-03-03 12:44:22 -0500

    add solution JavaCore task1120

--- 2.JavaCore/src/com/javarush/task/task11/task1120/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task11/task1120/Solution.java
@@ -8,15 +8,15 @@ public class Solution {
     public static void main(String[] args) {
     }
 
-    public class House extends Asset{
+    public class House extends Asset {
 
     }
 
-    public class Cat extends Animal{
+    public class Cat extends Animal {
 
     }
 
-    public class Car extends Asset{
+    public class Car extends Asset {
 
     }
 
