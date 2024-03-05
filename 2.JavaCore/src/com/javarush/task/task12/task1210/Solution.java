fbe896e7cb3310dcd809353b4ea90659590b626c
macbook <babayev.nicat@yandex.ru>
2024-03-04 21:21:36 -0500

    add solution JavaSyntax task1210

--- 2.JavaCore/src/com/javarush/task/task12/task1210/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task12/task1210/Solution.java
@@ -6,24 +6,17 @@ package com.javarush.task.task12.task1210;
 
 public class Solution {
     public static void main(String[] args) {
-
     }
 
-    public static int max(int i, int a) {
-        if (i > a) {
-            return i;
-        } else return a;
+    public static int max(int a, int b) {
+        return a > b ? a : b;
     }
 
-    public static long max(long i, long a) {
-        if (i > a) {
-            return i;
-        } else return a;
+    public static long max(long a, long b) {
+        return a > b ? a : b;
     }
 
-    public static double max(double i, double a) {
-        if (i > a) {
-            return i;
-        } else return a;
+    public static double max(double a, double b) {
+        return a > b ? a : b;
     }
 }
