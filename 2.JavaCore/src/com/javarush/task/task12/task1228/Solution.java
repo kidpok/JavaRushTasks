2e0cdb9e4279fceb116a07d99fb34fba3538cb4f
macbook <babayev.nicat@yandex.ru>
2024-03-05 20:08:43 -0500

    add solution JavaSyntax 1228

--- 2.JavaCore/src/com/javarush/task/task12/task1228/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task12/task1228/Solution.java
@@ -26,7 +26,7 @@ public class Solution {
         public void workVeryHard();
     }
 
-    public static class Human implements Worker, Businessman, Secretary{
+    public static class Human implements Secretary, Businessman, Worker {
 
         public void workHard() {
         }
